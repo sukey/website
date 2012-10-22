c77687ee4277903fbec4d6a80c2401c41a382ab4
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-10-22 06:08:22 +0200

    Update io/index.php
    turning off redirection for a while. Users can choose to browser to http://sukey.io/webapp

--- io/index.php
+++ io/index.php
@@ -1,7 +1,7 @@
 <?php
 
-include("detectMobile.php");
-if(isMobile()){header("Location: http://sukey.io/webapp/");}
+//include("detectMobile.php");
+//if(isMobile()){header("Location: http://sukey.io/webapp/");}
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
